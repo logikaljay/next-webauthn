0d263471563efede887b12c1d7b0ebfa9f23ce52
Jay Baker <logikal@gmail.com>
2023-05-10 13:00:51 +1200

    fix: check there are credentials before rendering anything

--- app/auth/login/password/page.tsx
+++ app/auth/login/password/page.tsx
@@ -1,10 +1,18 @@
 import { Separator } from "@/components/ui/separator";
 import { CredentialsForm } from "./credentials-form";
 import { PasskeyLogin } from "@/components/passkey";
+import { users } from "@/db/managers/users";
+import { redirect } from "next/navigation";
 
 export default async function CredentialsPage({ searchParams }) {
 
   const email = searchParams.email
+  const user = await users.getUserByEmail(email)
+  if (!user) {
+    return redirect('/auth/login?error=User not found')
+  }
+
+  const credentials = await users.getAllCredentialsForUserId(user.id)
 
   return (
     <div>
@@ -13,12 +21,16 @@ export default async function CredentialsPage({ searchParams }) {
         <form name="loginWithCredentials" className="space-y-4">
           <CredentialsForm />
           
-          <Separator className="my-6">
-            <div className="relative -top-2 mx-auto bg-secondary w-10 text-center text-muted-foreground text-xs font-mono">or</div>
-          </Separator>
-          
-          {/* @ts-ignore */}
-          <PasskeyLogin email={email} variant="outline" />
+          {credentials.length > 0 && (
+            <>
+              <Separator className="my-6">
+                <div className="relative -top-2 mx-auto bg-secondary w-10 text-center text-muted-foreground text-xs font-mono">or</div>
+              </Separator>
+              
+              {/* @ts-ignore */}
+              <PasskeyLogin email={email} variant="outline" />
+            </>
+          )}
         </form>
       </div>
     </div>
